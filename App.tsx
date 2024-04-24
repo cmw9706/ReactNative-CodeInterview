7504cf49d6b10bcfa8c1ba80c35afde5b0470822
Joe Gardener <joshgd626@gmail.com>
2024-04-24 11:24:31 -0500

    called the endpoint

--- App.tsx
+++ App.tsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {SafeAreaView, StyleSheet, Text} from 'react-native';
 
+
+
+const BASE_URL = 'https://rickandmortyapi.com/api/character'; 
 function App(): React.JSX.Element {
+
+  const [characters, getCharacters] = useState([]);
+
+
+  const getCharacters = async () => {
+
+    try {
+      const response = await fetch(BASE_URL);
+
+      console.log(response);
+  
+    } catch(e) {
+      console.log(e);
+    }
+  }
+
+  useEffect(() => {
+    getCharacters();
+  }, [])
+
+
   return (
     <SafeAreaView style={styles.container}>
       <Text>Code interview with connor!</Text>
